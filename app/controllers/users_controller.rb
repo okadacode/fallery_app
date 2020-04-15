cd9ce75c84662c0faeca9921e22a85bbef16802e
hiroki okada <thp.1414@gmail.com>
2020-04-16 06:39:05 +0900

    フレンドリーフォワーディングの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:notice] = "ログインが必要なページです"
         redirect_to login_url
       end
